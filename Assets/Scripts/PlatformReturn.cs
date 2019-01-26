96b4d5c87e62f4d66814f3b322504b542e0321a0
Unknown <dcarsten@uci.edu>
2019-01-26 11:50:45 -0800

    Updated Dane's scene

---
+++ Assets/Scripts/PlatformReturn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformReturn : MonoBehaviour
+{
+    public static int ResetX;
+    public static int ResetY;
+    public static int ResetZ;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
