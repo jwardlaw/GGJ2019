ded97006434e6752c2743c8f3a7dd4a96ff260c4
Unknown <dcarsten@uci.edu>
2019-01-26 10:46:13 -0800

    Built a Coin Pickup System

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    public GameObject Coin;
+    
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider other)
+    {
+        GlobalCoins.TotalCoins += 1;
+        Coin.GetComponent<Renderer>().enabled = false;
+        transform.GetChild(0).gameObject.SetActive(true);
+        //this.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/GlobalCoins.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GlobalCoins : MonoBehaviour
+{
+    public static int TotalCoins = 0;
+    public GameObject CoinCounter;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CoinCounter.GetComponent<Text>().text = "Coins: " + TotalCoins.ToString();
+    }
+}
