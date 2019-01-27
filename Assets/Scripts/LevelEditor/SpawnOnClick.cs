7282a17575bbcef43db64a3ed990abdb66fd0527
bslupik <bslupik@outlook.com>
2019-01-26 18:59:51 -0800

    Pushing latest

---
+++ Assets/Scripts/LevelEditor/SpawnOnClick.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnClick : MonoBehaviour
+{
+    LevelEditor levelEditor;
+
+    public void Start()
+    {
+        levelEditor = GameObject.Find("LevelEditor").GetComponent<LevelEditor>();
+    }
+
+    public void OnMouseDown()
+    {
+        print("OnMouseDown");
+
+        GameObject newObject = GameObject.Instantiate(levelEditor.objectToSpawn);
+        newObject.transform.SetPositionAndRotation(transform.position, transform.rotation);
+        GameObject.Destroy(gameObject);
+    }
+}
