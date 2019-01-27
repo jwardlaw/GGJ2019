33dcd1a9f1e6384d8dd7415c9e76f80ac1d933ae
Dane Carstens <dcarsten@uci.edu>
2019-01-26 16:48:05 -0800

    Made a hammer as an obstacle

---
+++ Assets/Scripts/MajorItemPickup1.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MajorItemPickup1 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        HouseInventory.Picture1 = true;
+    }
+}
