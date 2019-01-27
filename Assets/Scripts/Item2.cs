89840a2d4838c29d6e4f936f3a964f47c84bdbac
Dane Carstens <dcarsten@uci.edu>
2019-01-26 22:25:27 -0800

    Fixed Dane Scene

---
+++ Assets/Scripts/Item2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item2 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        HouseInventory.Picture2 = true;
+    }
+}
