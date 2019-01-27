2d9a83e26439ecebdb07b61a410afbac6b68da51
jwardlaw <jwardlaw@uci.edu>
2019-01-27 12:12:36 -0800

    more anims

--- Assets/Scripts/PlayerControl/PlayerAnimator.cs
+++ Assets/Scripts/PlayerControl/PlayerAnimator.cs
@@ -22,6 +22,13 @@ public class PlayerAnimator : MonoBehaviour
     void Update()
     {
         _anim.SetFloat("speed", _pc.currentSpeed);
+        _anim.SetBool("jump", Input.GetButtonDown("Jump"));
+        _anim.SetBool("falling", _pc.onGround == 0);
+        Debug.Log(_pc.onGround);
+        if(Input.GetButtonDown("Jump"))
+        {
+            _anim.SetTrigger("jump");
+        }
         transform.rotation = Quaternion.LookRotation(_pc.previousDirection);
     }
 
