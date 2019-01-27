8aa3aaf732ad0df80e29a13486e0ef3799078d35
jwardlaw <jwardlaw@uci.edu>
2019-01-26 22:19:21 -0800

    halfway implemented animation

--- Assets/Scripts/PlayerControl/PlayerAnimator.cs
+++ Assets/Scripts/PlayerControl/PlayerAnimator.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerAnimator : MonoBehaviour
 {
+    public PlayerController _pc;
+    public Rigidbody _rb;
+    public Animator _anim;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        _anim = GetComponent<Animator>();
+        _pc = transform.GetComponentInParent<PlayerController>();
+        _rb = _pc.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        _anim.SetFloat("speed", _pc.currentSpeed);
     }
 }
