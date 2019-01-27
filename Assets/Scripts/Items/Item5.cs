234ae11ff6b5a72cd880795337a7ea104a729cea
Dane Carstens <dcarsten@uci.edu>
2019-01-27 12:38:56 -0800

    Created a script for each major item and added a folder for them

---
+++ Assets/Scripts/Items/Item5.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item5 : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        HouseInventory.Picture5= true;
+    }
+}
