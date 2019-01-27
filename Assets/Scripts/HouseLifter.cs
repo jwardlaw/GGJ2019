646841025276558dc0663ed7c199eaed34e5ff24
Dane Carstens <dcarsten@uci.edu>
2019-01-27 10:29:30 -0800

    Built a house prefab

---
+++ Assets/Scripts/HouseLifter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseLifter : MonoBehaviour
+{
+    public bool Up = false;
+    public int Scale = 5;
+    void OnTriggerEnter(Collider Player)
+    {
+        Up = true;
+    }
+    void Update()
+    {
+        if (Up)
+        {
+            this.gameObject.transform.Translate(Vector3.up * (Scale * Time.deltaTime));
+        }
+    }
+}
