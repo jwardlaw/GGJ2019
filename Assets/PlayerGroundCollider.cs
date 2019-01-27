3e33a8fab51f5697aab33e4cfba20dab5359817f
bslupik <bslupik@outlook.com>
2019-01-27 09:08:49 -0800

    Imrpoved ground collision

---
+++ Assets/PlayerGroundCollider.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGroundCollider : MonoBehaviour
+{
+    public PlayerController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponentInParent<PlayerController>();
+    }
+
+    
+    public void OnTriggerEnter(Collider other)
+    {
+        print("OnTriggerEnter");
+        if (other.CompareTag("Ground"))
+        {
+            controller.OnGroundEnter();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        print("OnTriggerExit");
+        if (other.CompareTag("Ground"))
+        {
+            controller.OnGroundExit();
+        }
+    }
+    
+    /*
+    public void OnCollisionEnter(Collision other)
+    {
+        print("OnCollisionEnter");
+        if (other.collider.CompareTag("Ground"))
+        {
+            controller.OnGroundEnter();
+        }
+    }
+
+    public void OnCollisionExit(Collision other)
+    {
+        print("OnCollisionExit");
+        if (other.collider.CompareTag("Ground"))
+        {
+            controller.OnGroundExit();
+        }
+    }
+    */
+}
